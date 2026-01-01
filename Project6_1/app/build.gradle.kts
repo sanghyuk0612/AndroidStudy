d82c24cc86a2422e199336a1c05fe374882b6afd
sanghyuk0612 <hwhwhw944@gmail.com>
2026-01-01 16:40:39 +0900

    Project6_1 complete

--- Project6_1/app/build.gradle.kts
+++ Project6_1/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.cookandroid.project6_1"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.cookandroid.project6_1"
         minSdk = 33
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
