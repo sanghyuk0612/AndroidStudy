3352c0263656f6676d11660c6eeb68998e017469
sanghyuk0612 <hwhwhw944@gmail.com>
2026-01-14 22:31:08 +0900

    Project 10_1 complete

--- Project10_1/app/src/main/java/com/cookandroid/project10_1/MainActivity.java
+++ Project10_1/app/src/main/java/com/cookandroid/project10_1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.cookandroid.project10_1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button btnNewActivity = findViewById(R.id.btnNewActivity);
+        btnNewActivity.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
         });
     }
 }
