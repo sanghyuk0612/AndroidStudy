c64096160c096a683ae3c3a6d2f3100d130194aa
sanghyuk0612 <hwhwhw944@gmail.com>
2026-01-15 22:18:17 +0900

    Project 10_3 complete

--- Project10_3/app/src/main/java/com/cookandroid/project10_3/MainActivity.java
+++ Project10_3/app/src/main/java/com/cookandroid/project10_3/MainActivity.java
@@ -1,6 +1,11 @@
 package com.cookandroid.project10_3;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +18,62 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setTitle("액티비티 테스트 예제");
+        Log.i("액티비티 테스트", "onCreate() 호출됨");
+
+        Button btnDial = findViewById(R.id.btnDial);
+        Button btnFinish = findViewById(R.id.btnFinish);
+
+        btnDial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri  = Uri.parse("tel:01012345678");
+                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(intent);
+            }
         });
+        btnFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("액티비티 테스트", "onStop() 호출됨");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("액티비티 테스트", "onStart() 호출됨");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("액티비티 테스트", "onResume() 호출됨");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("액티비티 테스트", "onRestart() 호출됨");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("액티비티 테스트", "onPause() 호출됨");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("액티비티 테스트", "onDestroy() 호출됨");
     }
 }
